14df83d909d930ba6a57defc6fc51eb7d7d48a9d
arxcdr <34399111+arxcdr@users.noreply.github.com>
2018-11-02 10:36:08 +0100

    - Add zoom and better camera movement

--- 2D-RTS/Scripts/CameraMovement.gd
+++ 2D-RTS/Scripts/CameraMovement.gd
@@ -1,6 +1,13 @@
 extends Camera2D
 
-export var speed = 10.0
+export var speed = 19.0
+export var zoomSpeed = 10.0
+export var zoomMargin = 0.1
+export var zoomMin = 0.5
+export var zoomMax = 3.0
+
+var zoomPos = Vector2()
+var zoomFactor = 1.0
 
 func _ready():
 	pass
@@ -11,6 +18,29 @@ func _process(delta):
 	 - int(Input.is_action_pressed("ui_left")))
 	var inpy = (int(Input.is_action_pressed("ui_down"))
 	 - int(Input.is_action_pressed("ui_up")))
-	position.x += inpx*speed
-	position.y += inpy*speed
+	position.x = lerp(position.x, position.x + inpx * speed * zoom.x, speed * delta)
+	position.y = lerp(position.y, position.y + inpy * speed * zoom.y, speed * delta)
+	
+	#zoom in
+	zoom.x = lerp(zoom.x, zoom.x * zoomFactor, zoomSpeed * delta)
+	zoom.y = lerp(zoom.y, zoom.y * zoomFactor, zoomSpeed * delta)
+	
+	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
+	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
 	pass
+
+func _input(event):
+	if abs(zoomPos.x - get_global_mouse_position().x) > zoomMargin:
+		zoomFactor = 1.0
+	
+	if abs(zoomPos.y - get_global_mouse_position().y) > zoomMargin:
+		zoomFactor = 1.0
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoomFactor -= 0.01
+				zoomPos = get_global_mouse_position()
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				zoomFactor += 0.01
+				zoomPos = get_global_mouse_position()
