adf7f0b88e5b246b5f9a13f37aefcff71f3d2f36
arxcdr <34399111+arxcdr@users.noreply.github.com>
2018-11-03 18:36:49 +0100

    - Update Camera script to pan with the mouse

--- 2D-RTS/Scripts/CameraMovement.gd
+++ 2D-RTS/Scripts/CameraMovement.gd
@@ -1,11 +1,16 @@
 extends Camera2D
 
+export var panSpeed = 10.0
 export var speed = 19.0
 export var zoomSpeed = 10.0
 export var zoomMargin = 0.1
+
 export var zoomMin = 0.5
 export var zoomMax = 3.0
+export var marginX = 200.0
+export var marginY = 200.0
 
+var mousePos = Vector2()
 var zoomPos = Vector2()
 var zoomFactor = 1.0
 var zooming = false
@@ -21,6 +26,19 @@ func _process(delta):
 	 - int(Input.is_action_pressed("ui_up")))
 	position.x = lerp(position.x, position.x + inpx * speed * zoom.x, speed * delta)
 	position.y = lerp(position.y, position.y + inpy * speed * zoom.y, speed * delta)
+	
+	# Pan camera by dragging mouse to the sides while maintaining Ctrl key pressed
+	if Input.is_key_pressed(KEY_CONTROL):
+		#check mouse position
+		if mousePos.x < marginX:
+			position.x = lerp(position.x, position.x - abs(mousePos.x - marginX)/marginX * panSpeed * zoom.x, panSpeed * delta)
+		elif mousePos.x > OS.window_size.x - marginX:
+			position.x = lerp(position.x, position.x + abs(mousePos.x - OS.window_size.x + marginX)/marginX * panSpeed * zoom.x, panSpeed * delta)
+		if mousePos.y < marginY:
+			position.y = lerp(position.y, position.y - abs(mousePos.y - marginY)/marginY * panSpeed * zoom.y, panSpeed * delta)
+		elif mousePos.y > OS.window_size.y - marginY:
+			position.y = lerp(position.y, position.y + abs(mousePos.y - OS.window_size.y + marginY)/marginY * panSpeed * zoom.y, panSpeed * delta)
+	
 	
 	#zoom in
 	zoom.x = lerp(zoom.x, zoom.x * zoomFactor, zoomSpeed * delta)
@@ -44,4 +62,5 @@ func _input(event):
 				zoomPos = get_global_mouse_position()
 		else:
 			zooming = false
-			
+	if event is InputEventMouse:
+		mousePos = event.position
